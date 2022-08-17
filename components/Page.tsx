afb1f875aa48bf9c853525d391b9f6bd180e2243
4513ECHO <4513echo@gmail.com>
2022-08-18 08:50:01 +0900

    feat: Add header

--- components/Page.tsx
+++ components/Page.tsx
@@ -18,7 +18,7 @@ const LINKS = [
     href: "https://github.com/4513ECHO/aqua-picker/blob/main/LICENSE",
   },
   {
-    title: "Aqua Official Site",
+    title: "aqua Official Site",
     href: "https://aquaproj.github.io/",
   },
 ];
@@ -30,23 +30,36 @@ export function Page(props: AppProps) {
         <title>{props.title}</title>
         <meta property="og:title" content={props.title} />
       </Head>
-      <header>{/* TODO*/}</header>
+      <header
+        class={tw`bg-gray-100 h-16 flex flex-col text-gray-600`}
+      >
+        <a href="/" class={tw`p-3 flex`}>
+          <img
+            src="https://raw.githubusercontent.com/aquaproj/aqua/main/logo/aqua_without_text.svg"
+            alt="aqua Logo"
+            class={tw`h-12 w-12 px-2`}
+          />
+          <span class={tw`text-xl my-auto hover:underline`}>
+            Aqua Picker
+          </span>
+        </a>
+      </header>
       <main class={tw`p-4 max-w-screen-md mx-auto`}>{props.children}</main>
       <footer
         class={tw`bg-gray-100 h-32 flex flex-col gap-4 justify-center text(gray-600 center) sticky top-[100vh]`}
       >
-        <ul class={tw`flex justify-center gap-8`}>
-          {LINKS.map((link) => (
-            <li class={tw`hover:underline`}>
-              <a href={link.href}>{link.title}</a>
-            </li>
-          ))}
-        </ul>
+        <nav>
+          <ul class={tw`flex justify-center gap-8`}>
+            {LINKS.map((link) => (
+              <li class={tw`hover:underline`}>
+                <a href={link.href}>{link.title}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
         <p class={tw`mt-2`}>
-          Powered by{" "}
+          ©︎ {new Date().getFullYear()} Hibiki, built by{" "}
           <a href="https://fresh.deno.dev" class={tw`hover:underline`}>fresh</a>
-          <br />
-          ©︎ {new Date().getFullYear()} Hibiki
         </p>
       </footer>
     </div>
