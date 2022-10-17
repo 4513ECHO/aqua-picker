d78ca616f079b4e85b31429bf57045c59a410d98
4513ECHO <4513echo@gmail.com>
2022-10-17 16:39:52 +0900

    fix: Add missing file

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,18 +3,20 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/_middleware.ts";
-import * as $1 from "./routes/index.tsx";
-import * as $2 from "./routes/package/[...name].tsx";
-import * as $3 from "./routes/random.ts";
+import * as $0 from "./routes/_404.tsx";
+import * as $1 from "./routes/_middleware.ts";
+import * as $2 from "./routes/index.tsx";
+import * as $3 from "./routes/package/[...name].tsx";
+import * as $4 from "./routes/random.ts";
 import * as $$0 from "./islands/Search.tsx";
 
 const manifest = {
   routes: {
-    "./routes/_middleware.ts": $0,
-    "./routes/index.tsx": $1,
-    "./routes/package/[...name].tsx": $2,
-    "./routes/random.ts": $3,
+    "./routes/_404.tsx": $0,
+    "./routes/_middleware.ts": $1,
+    "./routes/index.tsx": $2,
+    "./routes/package/[...name].tsx": $3,
+    "./routes/random.ts": $4,
   },
   islands: {
     "./islands/Search.tsx": $$0,
